cd94ecfef94a6ec0010d1256fd3a4f3f142567ce
Arvind Kumar <suthararvindjlr757@gmail.com>
2024-06-05 18:45:39 +0530

    app.js

--- app.js
+++ app.js
@@ -2,64 +2,60 @@ import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import { connectPassport } from "./utils/Provider.js";
 import session from "express-session";
-import session from 'cookie-session';
 import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import cors from "cors";
 
-
-
-const app = express();
-export default app;
 dotenv.config({
   path: "./config/config.env",
 });
 
+const app = express();
+export default app;
+
 app.set("trust proxy", 1);
+
 // Using Middlewares
- app.use(
+app.use(
   session({
-     secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV !== "development",
+      httpOnly: process.env.NODE_ENV !== "development",
+      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    },
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  urlencoded({
+    extended: true,
+  })
+);
 
-   //  cookie: {
-   //    secure: process.env.NODE_ENV === "development" ? false : true,
-    //   httpOnly: process.env.NODE_ENV === "development" ? false : true,
-   //    sameSite: process.env.NODE_ENV === "development" ? false : "none",
-   //  },
-   })
- );
- app.use(cookieParser());
- app.use(express.json());
- app.use(
-   urlencoded({
-     extended: true,
-   })
- );
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
- app.use(
-   cors({
-     credentials: true,
-     origin: process.env.FRONTEND_URL,
-     methods: ["GET", "POST", "PUT", "DELETE"],
-   })
- );
+app.use(passport.initialize());
+app.use(passport.session());
 
- app.use(passport.authenticate("session"));
- app.use(passport.initialize());
- app.use(passport.session());
- app.enable("trust proxy");
+connectPassport();
 
- connectPassport();
-
- // Importing Routes
+// Importing Routes
 import userRoute from "./routes/user.js";
- import orderRoute from "./routes/order.js";
+import orderRoute from "./routes/order.js";
 
- app.use("/api/v1", userRoute);
- app.use("/api/v1", orderRoute);
+app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
 
- // Using Error Middleware
- app.use(errorMiddleware);
+// Using Error Middleware
+app.use(errorMiddleware);
